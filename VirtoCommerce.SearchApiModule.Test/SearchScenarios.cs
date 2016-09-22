dae28317dde7c683d8bb1a8eef870ccccd14ce5a
Tatarincev <et@virtoway.com>
2016-09-22 17:53:39 +0200

    Update VirtoCommerce.SearchModule.Data nuget dependency

--- VirtoCommerce.SearchApiModule.Test/SearchScenarios.cs
+++ VirtoCommerce.SearchApiModule.Test/SearchScenarios.cs
@@ -100,10 +100,9 @@ namespace VirtoCommerce.SearchModule.Tests
             provider.RemoveAll(scope, "sometype");
 
             // create bad connection
-            var queryBuilder = new ElasticSearchQueryBuilder();
 
             var conn = new SearchConnection("localhost:9201", scope);
-            var bad_provider = new ElasticSearchProvider(queryBuilder, conn);
+            var bad_provider = new ElasticSearchProvider(conn);
             bad_provider.EnableTrace = true;
 
             Assert.Throws<ElasticSearchException>(() => bad_provider.RemoveAll(badscope, ""));
