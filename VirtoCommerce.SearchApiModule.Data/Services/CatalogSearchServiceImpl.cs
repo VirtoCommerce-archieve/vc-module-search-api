683b9c05c3ce70bbdb62eab06d1c553695e1005b
Alexander Siniouguine <sasha@virtoway.com>
2016-10-21 20:07:10 -0700

    return outlines and don't do double conversions

--- VirtoCommerce.SearchApiModule.Data/Services/CatalogSearchServiceImpl.cs
+++ VirtoCommerce.SearchApiModule.Data/Services/CatalogSearchServiceImpl.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using VirtoCommerce.CatalogModule.Data.Repositories;
 using VirtoCommerce.CatalogModule.Web.Converters;
@@ -7,6 +8,9 @@ using VirtoCommerce.Domain.Catalog.Services;
 using VirtoCommerce.Platform.Core.Assets;
 using VirtoCommerce.SearchApiModule.Data.Model;
 using VirtoCommerce.SearchModule.Core.Model;
+using VirtoCommerce.SearchModule.Core.Model.Indexing;
+using VirtoCommerce.SearchModule.Core.Model.Search;
+using VirtoCommerce.SearchModule.Core.Model.Search.Criterias;
 
 namespace VirtoCommerce.SearchApiModule.Data.Services
 {
@@ -19,6 +23,8 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
         private readonly IItemBrowsingService _browseService;
         private readonly ISearchConnection _searchConnection;
         private readonly IBlobUrlResolver _blobUrlResolver;
+        private readonly ISearchProvider _searchProvider;
+        private readonly IItemService _itemService;
 
         public CatalogSearchServiceImpl(Func<ICatalogRepository> catalogRepositoryFactory, 
             IItemService itemService, 
@@ -26,13 +32,16 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
             ICategoryService categoryService,
             IItemBrowsingService browseService,
             ISearchConnection searchConnection,
-            IBlobUrlResolver blobUrlResolver
+            IBlobUrlResolver blobUrlResolver,
+            ISearchProvider searchService
             )
         {
             _catalogSearchServiceImpl_Catalog = new CatalogModule.Data.Services.CatalogSearchServiceImpl(catalogRepositoryFactory, itemService, catalogService, categoryService);
             _browseService = browseService;
             _searchConnection = searchConnection;
             _blobUrlResolver = blobUrlResolver;
+            _searchProvider = searchService;
+            _itemService = itemService;
         }
 
         public SearchResult Search(SearchCriteria criteria)
@@ -59,17 +68,77 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
                     WithHidden = true
                 };
 
-                var searchResults = _browseService.SearchItems(_searchConnection.Scope, serviceCriteria, ItemResponseGroup.ItemInfo | ItemResponseGroup.Outlines);
-
-                if(searchResults.Products != null)
-                {
-                    retVal.Products = searchResults.Products.Select(x => x.ToModuleModel(_blobUrlResolver)).ToArray();
-                }
-
-                retVal.ProductsTotalCount = (int)searchResults.TotalCount;
+                SearchItems(_searchConnection.Scope, retVal, serviceCriteria, ItemResponseGroup.ItemInfo | ItemResponseGroup.Outlines);
             }
 
             return retVal;
+        }
+
+        public void SearchItems(string scope, SearchResult results, ISearchCriteria criteria, ItemResponseGroup responseGroup)
+        {
+            var items = new List<CatalogProduct>();
+            var itemsOrderedList = new List<string>();
+
+            var foundItemCount = 0;
+            var dbItemCount = 0;
+            var searchRetry = 0;
+
+            //var myCriteria = criteria.Clone();
+            var myCriteria = criteria;
+
+            ISearchResults<DocumentDictionary> searchResults = null;
+
+            do
+            {
+                // Search using criteria, it will only return IDs of the items
+                searchResults = _searchProvider.Search<DocumentDictionary>(scope, criteria);
+
+                searchRetry++;
+
+                if (searchResults == null || searchResults.Documents == null)
+                {
+                    continue;
+                }
+
+                //Get only new found itemIds
+                var uniqueKeys = searchResults.Documents.Select(x => x.Id.ToString()).Except(itemsOrderedList).ToArray();
+                foundItemCount = uniqueKeys.Length;
+
+                if (!searchResults.Documents.Any())
+                {
+                    continue;
+                }
+
+                itemsOrderedList.AddRange(uniqueKeys);
+
+                // if we can determine catalog, pass it to the service
+                string catalog = null;
+                if (criteria is CatalogItemSearchCriteria)
+                {
+                    catalog = (criteria as CatalogItemSearchCriteria).Catalog;
+                }
+
+                // Now load items from repository
+                var currentItems = _itemService.GetByIds(uniqueKeys.ToArray(), responseGroup, catalog);
+
+                var orderedList = currentItems.OrderBy(i => itemsOrderedList.IndexOf(i.Id));
+                items.AddRange(orderedList);
+                dbItemCount = currentItems.Length;
+
+                //If some items where removed and search is out of sync try getting extra items
+                if (foundItemCount > dbItemCount)
+                {
+                    //Retrieve more items to fill missing gap
+                    myCriteria.RecordsToRetrieve += (foundItemCount - dbItemCount);
+                }
+            }
+            while (foundItemCount > dbItemCount && searchResults != null && searchResults.Documents.Any() && searchRetry <= 3 &&
+                (myCriteria.RecordsToRetrieve + myCriteria.StartingRecord) < searchResults.TotalCount);
+
+            results.Products = items.ToArray();
+
+            if (searchResults != null)
+                results.ProductsTotalCount = (int)searchResults.TotalCount;
         }
     }
 }
