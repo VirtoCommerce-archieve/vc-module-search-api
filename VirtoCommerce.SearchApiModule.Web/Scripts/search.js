0518fac54c7d7f98d54b82b2de7f85faa506d5a8
eMazeika <em@virtoway.com>
2016-10-28 18:38:04 +0300

    catalog predefined Filters added

--- VirtoCommerce.SearchApiModule.Web/Scripts/search.js
+++ VirtoCommerce.SearchApiModule.Web/Scripts/search.js
@@ -4,15 +4,21 @@ if (AppDependencies != undefined) {
     AppDependencies.push(moduleName);
 }
 
-angular.module(moduleName, [
-    'ngSanitize'
-])
+angular.module(moduleName, ['ngSanitize', 'virtoCommerce.catalogModule'])
 .run(
-  ['platformWebApp.widgetService', function (widgetService) {
+  ['platformWebApp.widgetService', 'virtoCommerce.catalogModule.predefinedSearchFilters', function (widgetService, predefinedSearchFilters) {
       // filter properties in STORE details
       widgetService.registerWidget({
           controller: 'virtoCommerce.searchModuleApi.storePropertiesWidgetController',
           template: 'Modules/$(VirtoCommerce.SearchApi)/Scripts/widgets/storePropertiesWidget.tpl.html'
       }, 'storeDetail');
+
+      // predefine search filters for catalog search
+      predefinedSearchFilters.register(1477584000000, 'catalogSearchFiltersDate-searchAPI', [
+        { keyword: 'is:hidden', id: 4, name: 'catalog.blades.categories-items-list.labels.filter-notActive' },
+        { keyword: 'price_usd:[100 TO 200]', id: 3, name: 'catalog.blades.categories-items-list.labels.filter-priceRange' },
+        { keyword: 'is:priced', id: 2, name: 'catalog.blades.categories-items-list.labels.filter-withPrice' },
+        { keyword: 'is:unpriced', id: 1, name: 'catalog.blades.categories-items-list.labels.filter-priceless' }
+      ]);
   }]
 );
