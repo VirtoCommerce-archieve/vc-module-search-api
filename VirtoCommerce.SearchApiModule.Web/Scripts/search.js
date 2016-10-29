3dad4fd99722edb971fe7a4921c05e2409bfda58
Tatarincev <et@virtoway.com>
2016-10-29 08:34:07 +0200

    Move some localization resources from Catalog module

--- VirtoCommerce.SearchApiModule.Web/Scripts/search.js
+++ VirtoCommerce.SearchApiModule.Web/Scripts/search.js
@@ -15,10 +15,10 @@ angular.module(moduleName, ['ngSanitize', 'virtoCommerce.catalogModule'])
 
       // predefine search filters for catalog search
       predefinedSearchFilters.register(1477584000000, 'catalogSearchFiltersDate-searchAPI', [
-        { keyword: 'is:hidden', id: 4, name: 'catalog.blades.categories-items-list.labels.filter-notActive' },
-        { keyword: 'price_usd:[100 TO 200]', id: 3, name: 'catalog.blades.categories-items-list.labels.filter-priceRange' },
-        { keyword: 'is:priced', id: 2, name: 'catalog.blades.categories-items-list.labels.filter-withPrice' },
-        { keyword: 'is:unpriced', id: 1, name: 'catalog.blades.categories-items-list.labels.filter-priceless' }
+        { keyword: 'is:hidden', id: 4, name: 'searchApi.filter-labels.filter-notActive' },
+        { keyword: 'price_usd:[100 TO 200]', id: 3, name: 'searchApi.filter-labels.filter-priceRange' },
+        { keyword: 'is:priced', id: 2, name: 'searchApi.filter-labels.filter-withPrice' },
+        { keyword: 'is:unpriced', id: 1, name: 'searchApi.filter-labels.filter-priceless' }
       ]);
   }]
 );
