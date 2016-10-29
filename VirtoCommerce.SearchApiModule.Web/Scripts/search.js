49a14aa470511e4294d8e6925b1c49d02456643b
Tatarincev <et@virtoway.com>
2016-10-29 09:32:31 +0200

    Move catalog index UI element from search.module

--- VirtoCommerce.SearchApiModule.Web/Scripts/search.js
+++ VirtoCommerce.SearchApiModule.Web/Scripts/search.js
@@ -6,12 +6,63 @@ if (AppDependencies != undefined) {
 
 angular.module(moduleName, ['ngSanitize', 'virtoCommerce.catalogModule'])
 .run(
-  ['platformWebApp.widgetService', 'virtoCommerce.catalogModule.predefinedSearchFilters', function (widgetService, predefinedSearchFilters) {
+  ['platformWebApp.widgetService', 'virtoCommerce.catalogModule.predefinedSearchFilters', 'platformWebApp.toolbarService', 'virtoCommerce.searchModule.search', 'platformWebApp.dialogService', 'platformWebApp.bladeNavigationService', function (widgetService, predefinedSearchFilters, toolbarService, searchAPI, dialogService, bladeNavigationService) {
       // filter properties in STORE details
       widgetService.registerWidget({
           controller: 'virtoCommerce.searchModuleApi.storePropertiesWidgetController',
           template: 'Modules/$(VirtoCommerce.SearchApi)/Scripts/widgets/storePropertiesWidget.tpl.html'
       }, 'storeDetail');
+
+      // toolbar button 'rebuild'
+      var rebuildIndexCommand = {
+          name: "search.commands.rebuild-index",
+          icon: 'fa fa-recycle',
+          index: 2,
+          executeMethod: function (blade) {
+              var dialog = {
+                  id: "confirmRebuildIndex",
+                  callback: function (doReindex) {
+                      var apiToCall = doReindex ? searchAPI.reindex : searchAPI.index;
+                      var documentsIds = blade.currentEntityId ? [{ id: blade.currentEntityId }] : undefined;
+                      apiToCall({ documentType: blade.documentType }, documentsIds,
+                              function openProgressBlade(data) {
+                                  // show indexing progress
+                                  var newBlade = {
+                                      id: 'indexProgress',
+                                      notification: data,
+                                      parentRefresh: blade.parentRefresh,
+                                      controller: 'virtoCommerce.searchModule.indexProgressController',
+                                      template: 'Modules/$(VirtoCommerce.Search)/Scripts/blades/index-progress.tpl.html'
+                                  };
+                                  bladeNavigationService.showBlade(newBlade, blade.parentBlade || blade);
+                              });
+                  }
+              }
+              dialogService.showDialog(dialog, 'Modules/$(VirtoCommerce.SearchApi)/Scripts/dialogs/reindex-dialog.tpl.html', 'platformWebApp.confirmDialogController');
+          },
+          canExecuteMethod: function () { return true; },
+          permission: 'VirtoCommerce.Search:Index:Rebuild'
+      };
+
+      // register in catalogs list
+      toolbarService.register(rebuildIndexCommand, 'virtoCommerce.catalogModule.catalogsListController');
+
+      // register WIDGETS
+      var indexWidget = {
+          controller: 'virtoCommerce.searchModule.indexWidgetController',
+          // size: [3, 1],
+          template: 'Modules/$(VirtoCommerce.Search)/Scripts/widgets/common/index-widget.tpl.html'
+      };
+
+      // integration: index in product details
+      var widgetToRegister = angular.extend({}, indexWidget, { documentType: 'catalogitem' })
+      widgetService.registerWidget(widgetToRegister, 'itemDetail');
+      // integration: index in CATEGORY details
+      widgetToRegister = angular.extend({}, indexWidget, { documentType: 'category' })
+      widgetService.registerWidget(widgetToRegister, 'categoryDetail');
+      // integration: index in catalog details
+      //widgetToRegister = angular.extend({}, indexWidget, { documentType: 'catalog' })
+      //widgetService.registerWidget(widgetToRegister, 'catalogDetail');
 
       // predefine search filters for catalog search
       predefinedSearchFilters.register(1477584000000, 'catalogSearchFiltersDate-searchAPI', [
