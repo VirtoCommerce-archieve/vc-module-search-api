2f089c9cc1f5bb570a276822b57aeb233f4578df
artem-dudarev <ad@virtoway.com>
2016-12-06 16:26:51 +0200

    Reduce search results according to response group

--- VirtoCommerce.SearchApiModule.Data/Services/ItemBrowsingService.cs
+++ VirtoCommerce.SearchApiModule.Data/Services/ItemBrowsingService.cs
@@ -1,21 +1,20 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using VirtoCommerce.CatalogModule.Web.Converters;
 using VirtoCommerce.Domain.Catalog.Model;
 using VirtoCommerce.Domain.Catalog.Services;
 using VirtoCommerce.Platform.Core.Assets;
-using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.SearchApiModule.Data.Converters;
 using VirtoCommerce.SearchApiModule.Data.Model;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Core.Model.Indexing;
-using VirtoCommerce.SearchModule.Core.Model.Search;
 using VirtoCommerce.SearchModule.Core.Model.Search.Criterias;
 using VirtoCommerce.SearchModule.Data.Services;
-using VirtoCommerce.Platform.Core.Settings;
+using catalogModel = VirtoCommerce.CatalogModule.Web.Model;
 
 namespace VirtoCommerce.SearchApiModule.Data.Services
 {
@@ -24,7 +23,7 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
         private readonly IItemService _itemService;
         private readonly ISearchProvider _searchProvider;
         private readonly IBlobUrlResolver _blobUrlResolver;
-        private readonly  ISettingsManager _settingsManager;
+        private readonly ISettingsManager _settingsManager;
 
         public ItemBrowsingService(IItemService itemService,
             ISearchProvider searchService, IBlobUrlResolver blobUrlResolver, ISettingsManager settingsManager)
@@ -39,71 +38,125 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
         {
             var response = new ProductSearchResult();
             var searchResults = _searchProvider.Search<DocumentDictionary>(scope, criteria);
-
-            if (searchResults != null && searchResults.Documents != null)
-            {               
-                var productDtos = new ConcurrentBag<CatalogModule.Web.Model.Product>();
-                var taskList = new List<Task>();
-
-                var documents = searchResults.Documents;             
-                if (_settingsManager.GetValue("VirtoCommerce.SearchApi.UseFullObjectIndexStoring", true))
-                {
-                    var fullIndexedDocuments = documents.Where(x => x.ContainsKey("__object") && !string.IsNullOrEmpty(x["__object"].ToString()));
-                    documents = documents.Except(fullIndexedDocuments);
-                    var deserializeProductsTask = System.Threading.Tasks.Task.Factory.StartNew(() =>
-                    {                       
-                        Parallel.ForEach(fullIndexedDocuments, new ParallelOptions { MaxDegreeOfParallelism = 5 }, (x) =>
-                        {
-                            var jsonProduct = x["__object"].ToString();
-                            var product = JsonConvert.DeserializeObject(jsonProduct, typeof(CatalogModule.Web.Model.Product)) as CatalogModule.Web.Model.Product;
-                            productDtos.Add(product);
-                        });
-                    });
-                    taskList.Add(deserializeProductsTask);
-                }
-
-                if (documents.Any())
-                {
-                    var loadProductsTask = System.Threading.Tasks.Task.Factory.StartNew(() =>
-                    {
-                        string catalog = null;
-                        if (criteria is CatalogItemSearchCriteria)
-                        {
-                            catalog = (criteria as CatalogItemSearchCriteria).Catalog;
-                        }
-                        var productIds = documents.Select(x => x.Id.ToString()).Distinct().ToArray();
-                        if (productIds.Any())
-                        {
-                        // Now load items from repository
-                        var products = _itemService.GetByIds(productIds, responseGroup, catalog);
-                            Parallel.ForEach(products, (x) =>
-                            {
-                                productDtos.Add(x.ToWebModel(_blobUrlResolver));
-                            });
-                        }
-                    }
-                    );
-                    taskList.Add(loadProductsTask);
-                }
-
-                Task.WaitAll(taskList.ToArray());
-
-                //Preserver original sorting order
-                var orderedIds = searchResults.Documents.Select(x => x.Id.ToString()).ToList();
-                response.Products = productDtos.OrderBy(i => orderedIds.IndexOf(i.Id)).ToArray();
-            }
-          
-
             if (searchResults != null)
+            {
                 response.TotalCount = searchResults.TotalCount;
 
-            // TODO need better way to find applied filter values
-            var appliedFilters = criteria.CurrentFilters.SelectMany(x => x.GetValues()).Select(x => x.Id).ToArray();
-            if (searchResults.Facets != null)
-            {
-                response.Aggregations = searchResults.Facets.Select(g => g.ToModuleModel(appliedFilters)).ToArray();
+                if (searchResults.Documents != null)
+                {
+                    var productDtos = new ConcurrentBag<catalogModel.Product>();
+                    var taskList = new List<Task>();
+
+                    var documents = searchResults.Documents.ToList();
+
+                    if (_settingsManager.GetValue("VirtoCommerce.SearchApi.UseFullObjectIndexStoring", true))
+                    {
+                        var fullIndexedDocuments = documents.Where(x => x.ContainsKey("__object") && !string.IsNullOrEmpty(x["__object"].ToString())).ToList();
+                        documents = documents.Except(fullIndexedDocuments).ToList();
+
+                        var deserializeProductsTask = Task.Factory.StartNew(() =>
+                        {
+                            Parallel.ForEach(fullIndexedDocuments, new ParallelOptions { MaxDegreeOfParallelism = 5 }, doc =>
+                            {
+                                var jsonProduct = doc["__object"].ToString();
+                                var product = JsonConvert.DeserializeObject(jsonProduct, typeof(catalogModel.Product)) as catalogModel.Product;
+                                ReduceSearchResult(criteria, responseGroup, product);
+                                productDtos.Add(product);
+                            });
+                        });
+
+                        taskList.Add(deserializeProductsTask);
+                    }
+
+                    if (documents.Any())
+                    {
+                        var loadProductsTask = Task.Factory.StartNew(() =>
+                            {
+                                string catalog = null;
+                                var catalogItemSearchCriteria = criteria as CatalogItemSearchCriteria;
+                                if (catalogItemSearchCriteria != null)
+                                {
+                                    catalog = catalogItemSearchCriteria.Catalog;
+                                }
+
+                                var productIds = documents.Select(x => x.Id.ToString()).Distinct().ToArray();
+                                if (productIds.Any())
+                                {
+                                    // Now load items from repository
+                                    var products = _itemService.GetByIds(productIds, responseGroup, catalog);
+                                    Parallel.ForEach(products, p =>
+                                    {
+                                        var product = p.ToWebModel(_blobUrlResolver);
+                                        ReduceSearchResult(criteria, responseGroup, product);
+                                        productDtos.Add(product);
+                                    });
+                                }
+                            }
+                        );
+
+                        taskList.Add(loadProductsTask);
+                    }
+
+                    Task.WaitAll(taskList.ToArray());
+
+                    //Preserver original sorting order
+                    var orderedIds = searchResults.Documents.Select(x => x.Id.ToString()).ToList();
+                    response.Products = productDtos.OrderBy(i => orderedIds.IndexOf(i.Id)).ToArray();
+                }
+
+                if (searchResults.Facets != null)
+                {
+                    // TODO: need better way to find applied filter values
+                    var appliedFilters = criteria.CurrentFilters.SelectMany(x => x.GetValues()).Select(x => x.Id).ToArray();
+                    response.Aggregations = searchResults.Facets.Select(g => g.ToModuleModel(appliedFilters)).ToArray();
+                }
             }
+
             return response;
+        }
+
+
+        protected virtual void ReduceSearchResult(ISearchCriteria criteria, ItemResponseGroup responseGroup, catalogModel.Product product)
+        {
+            if (!responseGroup.HasFlag(ItemResponseGroup.ItemAssets))
+            {
+                product.Assets = null;
+            }
+
+            if (!responseGroup.HasFlag(ItemResponseGroup.ItemAssociations))
+            {
+                product.Associations = null;
+            }
+
+            if (!responseGroup.HasFlag(ItemResponseGroup.ItemEditorialReviews))
+            {
+                product.Reviews = null;
+            }
+
+            if (!responseGroup.HasFlag(ItemResponseGroup.ItemProperties))
+            {
+                product.Properties = null;
+            }
+
+            if (!responseGroup.HasFlag(ItemResponseGroup.Links))
+            {
+                product.Links = null;
+            }
+
+            if (!responseGroup.HasFlag(ItemResponseGroup.Outlines))
+            {
+                product.Outlines = null;
+            }
+
+            if (!responseGroup.HasFlag(ItemResponseGroup.Seo))
+            {
+                product.SeoInfos = null;
+            }
+
+            if (!responseGroup.HasFlag(ItemResponseGroup.Variations))
+            {
+                product.Variations = null;
+            }
         }
     }
 }
