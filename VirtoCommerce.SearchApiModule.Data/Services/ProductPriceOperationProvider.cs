1235ca2243b3035c9464133c903244753f89eca0
artem-dudarev <ad@virtoway.com>
2017-05-12 15:15:49 +0200

    Simplified getting operations for changed prices

--- VirtoCommerce.SearchApiModule.Data/Services/ProductPriceOperationProvider.cs
+++ VirtoCommerce.SearchApiModule.Data/Services/ProductPriceOperationProvider.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using VirtoCommerce.Domain.Pricing.Model;
 using VirtoCommerce.Domain.Pricing.Services;
 using VirtoCommerce.Platform.Core.ChangeLog;
 using VirtoCommerce.Platform.Core.Common;
@@ -27,15 +26,19 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
 
         public IList<Operation> GetOperations(DateTime startDate, DateTime endDate)
         {
-            var allPriceChanges = _changeLogService.FindChangeHistory("PriceEntity", startDate, endDate).ToList();
-            var priceIds = allPriceChanges.Select(c => c.ObjectId).ToArray();
-            var prices = GetPrices(priceIds);
+            var allOperations = _changeLogService.FindChangeHistory("PriceEntity", startDate, endDate).ToList();
+            var priceIds = allOperations.Select(c => c.ObjectId).ToArray();
+            var priceIdsAndProductIds = GetProductIds(priceIds);
 
             // TODO: How to get product for deleted price?
-            var result = allPriceChanges
-                .Select(c => new { Timestamp = c.ModifiedDate ?? c.CreatedDate, Price = prices.ContainsKey(c.ObjectId) ? prices[c.ObjectId] : null })
-                .Where(x => x.Price != null)
-                .Select(x => new Operation { ObjectId = x.Price.ProductId, Timestamp = x.Timestamp, OperationType = OperationType.Index })
+            var result = allOperations
+                .Where(o => priceIdsAndProductIds.ContainsKey(o.ObjectId))
+                .Select(o => new Operation
+                {
+                    ObjectId = priceIdsAndProductIds[o.ObjectId],
+                    Timestamp = o.ModifiedDate ?? o.CreatedDate,
+                    OperationType = OperationType.Index,
+                })
                 .GroupBy(o => o.ObjectId)
                 .Select(g => g.OrderByDescending(o => o.Timestamp).First())
                 .ToList();
@@ -44,19 +47,19 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
         }
 
 
-        protected virtual IDictionary<string, Price> GetPrices(ICollection<string> priceIds)
+        protected virtual IDictionary<string, string> GetProductIds(ICollection<string> priceIds)
         {
             // TODO: Get pageSize and degreeOfParallelism from settings
-            return GetPricesWithPagingAndParallelism(priceIds, 1000, 10);
+            return GetProductIdsWithPagingAndParallelism(priceIds, 1000, 10);
         }
 
-        protected virtual IDictionary<string, Price> GetPricesWithPagingAndParallelism(ICollection<string> priceIds, int pageSize, int degreeOfParallelism)
+        protected virtual IDictionary<string, string> GetProductIdsWithPagingAndParallelism(ICollection<string> priceIds, int pageSize, int degreeOfParallelism)
         {
-            IDictionary<string, Price> result;
+            IDictionary<string, string> result;
 
             if (degreeOfParallelism > 1)
             {
-                var dictionary = new ConcurrentDictionary<string, Price>();
+                var dictionary = new ConcurrentDictionary<string, string>();
 
                 var pages = new List<string[]>();
                 priceIds.ProcessWithPaging(pageSize, (ids, skipCount, totalCount) => pages.Add(ids.ToArray()));
@@ -68,7 +71,8 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
                     var prices = _pricingService.GetPricesById(ids);
                     foreach (var price in prices)
                     {
-                        dictionary.AddOrUpdate(price.Id, price, (key, oldValue) => price);
+                        var productId = price.ProductId;
+                        dictionary.AddOrUpdate(price.Id, productId, (key, oldValue) => productId);
                     }
                 });
 
@@ -76,13 +80,13 @@ namespace VirtoCommerce.SearchApiModule.Data.Services
             }
             else
             {
-                var dictionary = new Dictionary<string, Price>();
+                var dictionary = new Dictionary<string, string>();
 
                 priceIds.ProcessWithPaging(pageSize, (ids, skipCount, totalCount) =>
                 {
                     foreach (var price in _pricingService.GetPricesById(ids.ToArray()))
                     {
-                        dictionary[price.Id] = price;
+                        dictionary[price.Id] = price.ProductId;
                     }
                 });
 
