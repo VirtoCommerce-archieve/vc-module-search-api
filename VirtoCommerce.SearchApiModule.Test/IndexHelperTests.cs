57bab22712fd4f0e85fd4fe2e29f1cf3836278bf
artem-dudarev <ad@virtoway.com>
2017-06-09 16:01:16 +0200

    Include default values when serializing to JSON

---
+++ VirtoCommerce.SearchApiModule.Test/IndexHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using VirtoCommerce.SearchApiModule.Data.Helpers;
+using Xunit;
+
+namespace VirtoCommerce.SearchApiModule.Test
+{
+    [Trait("Category", "CI")]
+    public class IndexHelperTests
+    {
+        private class MyClass
+        {
+            public bool Boolean { get; set; }
+            public int Integer { get; set; }
+            public MyClass Reference { get; set; }
+        }
+
+        [Fact]
+        public void Serialize()
+        {
+            var obj = new MyClass
+            {
+                Boolean = true,
+                Integer = 1,
+                Reference = new MyClass(),
+            };
+
+            var result = IndexHelper.SerializeObject(obj);
+
+            Assert.Equal("{\"Boolean\":true,\"Integer\":1,\"Reference\":{\"Boolean\":false,\"Integer\":0}}", result);
+        }
+    }
+}
